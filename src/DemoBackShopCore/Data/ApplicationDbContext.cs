1eb3d7ac27357a0ff8186ed9a174f262f920e5bc
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-03-21 12:10:20 -0300

    feat: add DbContext configuration and OnConfiguring method

--- src/DemoBackShopCore/Data/ApplicationDbContext.cs
+++ src/DemoBackShopCore/Data/ApplicationDbContext.cs
@@ -1,7 +1,26 @@
+using DemoBackShopCore.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace DemoBackShopCore.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
     {
-        
+        public DbSet<Customer> Customers { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options: options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=localhost;Database=DemoBackShopCore;User=SA;Password=Password123!;TrustServerCertificate=True";
+            optionsBuilder.UseSqlServer(connectionString: connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
--- src/DemoBackShopCore/Data/IApplicationDbContext.cs
+++ src/DemoBackShopCore/Data/IApplicationDbContext.cs
@@ -1,7 +1,11 @@
+using DemoBackShopCore.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace DemoBackShopCore.Data
 {
     public interface IApplicationDbContext
     {
-        
+        public DbSet<Customer> Customers { get; set; }
+        public int SaveChanges();
     }
 }
