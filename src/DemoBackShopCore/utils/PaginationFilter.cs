7d4805e65ec4bc037bc33a9fc92fafb3b5d64d28
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-03-28 12:36:09 -0300

    feat: Add PaginationFilter

---
+++ src/DemoBackShopCore/utils/PaginationFilter.cs
@@ -0,0 +1,14 @@
+namespace DemoBackShopCore.Utils
+{
+    public class PaginationFilter
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+
+        public PaginationFilter(int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber < 1 ? 1 : pageNumber;
+            PageSize = pageSize;
+        }
+    }
+}
