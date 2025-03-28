1d5f680bc4c183aaed7482cd89846bd50408380c
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-03-28 11:55:01 -0300

    feat: Add Customer Controller

---
+++ src/DemoBackShopCore/Controllers/CustomersController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoBackShopCore.Controllers
+{
+    [ApiController]
+    [Route("api/Customers")]
+    public class CustomersController : ControllerBase
+    {
+        
+    }
+}
