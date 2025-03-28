0962351dcd0286626644e4db9893cadb16b3fab7
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-03-28 12:31:29 -0300

    feat: Implemented Interface IRepoistoryBase

--- src/DemoBackShopCore/Repository/IRepositoryBase.cs
+++ src/DemoBackShopCore/Repository/IRepositoryBase.cs
@@ -2,6 +2,12 @@ namespace DemoBackShopCore.Repository
 {
     public interface IRepositoryBase<TEntity> where TEntity : class
     {
-
+        public IQueryable<TEntity> GetAll();
+        public TEntity GetById(int id);
+        public TEntity Add(TEntity entity);
+        public void AddRange(IEnumerable<TEntity> entities);
+        public void Update(int id, TEntity entity);
+        public void Remove(int id);
+        public void SaveChanges();
     } 
 }
--- src/DemoBackShopCore/Repository/RepositoryBase.cs
+++ src/DemoBackShopCore/Repository/RepositoryBase.cs
@@ -1,7 +1,52 @@
+using DemoBackShopCore.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace DemoBackShopCore.Repository
 {
     public class RepositoryBase<TEntity> : IRepositoryBase<TEntity> where TEntity : class
     {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly DbSet<TEntity> _dbSetEntity;
 
+        public RepositoryBase(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSetEntity = _dbContext.Set<TEntity>();
+        }
+
+        public TEntity Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
