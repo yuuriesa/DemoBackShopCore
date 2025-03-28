ef76bf66d7e6d96cc6a6ebe2ecc0871a77037af1
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-03-28 13:09:53 -0300

    feat: Generate Constructor for the Rpository Customer

--- src/DemoBackShopCore/Repository/CustomerRepository.cs
+++ src/DemoBackShopCore/Repository/CustomerRepository.cs
@@ -1,9 +1,12 @@
+using DemoBackShopCore.Data;
 using DemoBackShopCore.Models;
 
 namespace DemoBackShopCore.Repository
 {
     public class CustomerRepository : RepositoryBase<Customer>, ICustomerRepository
     {
-        
+        public CustomerRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
     }
 }
