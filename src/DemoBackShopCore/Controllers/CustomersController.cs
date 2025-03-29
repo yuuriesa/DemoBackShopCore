f1e7e9bada46943988712448132dc5df6eec25d8
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-03-29 14:02:15 -0300

    feat: dependency injection made in Customer Controller and first method added.

--- src/DemoBackShopCore/Controllers/CustomersController.cs
+++ src/DemoBackShopCore/Controllers/CustomersController.cs
@@ -1,3 +1,4 @@
+using DemoBackShopCore.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DemoBackShopCore.Controllers
@@ -6,6 +7,16 @@ namespace DemoBackShopCore.Controllers
     [Route("api/Customers")]
     public class CustomersController : ControllerBase
     {
-        
+        private readonly ICustomerServices _services;
+        public CustomersController(ICustomerServices services)
+        {
+            _services = services;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
     }
 }
