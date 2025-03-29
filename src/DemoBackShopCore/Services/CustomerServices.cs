49b01f3b2568fd6036ccaf164bee686e935db930
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-03-29 13:57:00 -0300

    feat: Implemented interface service for customer

--- src/DemoBackShopCore/Services/CustomerServices.cs
+++ src/DemoBackShopCore/Services/CustomerServices.cs
@@ -1,10 +1,47 @@
+using DemoBackShopCore.DTOs;
 using DemoBackShopCore.Models;
 using DemoBackShopCore.Repository;
+using DemoBackShopCore.Utils;
 
 namespace DemoBackShopCore.Services
 {
     public class CustomerServices : ICustomerServices
     {
-        
+        private readonly ICustomerRepository _repository;
+
+        public CustomerServices(ICustomerRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Add(CustomerRequestDTO customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<CustomerRequestDTO> customers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Customer> GetAll(PaginationFilter paginationFilter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, CustomerRequestDTO customer)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- src/DemoBackShopCore/Services/ICustomerServices.cs
+++ src/DemoBackShopCore/Services/ICustomerServices.cs
@@ -1,10 +1,17 @@
+using DemoBackShopCore.DTOs;
 using DemoBackShopCore.Models;
 using DemoBackShopCore.Repository;
+using DemoBackShopCore.Utils;
 
 namespace DemoBackShopCore.Services
 {
     public interface ICustomerServices
     {
-
+        public IQueryable<Customer> GetAll(PaginationFilter paginationFilter);
+        public Customer GetById(int id);
+        public void Add(CustomerRequestDTO customer);
+        public void AddRange(IEnumerable<CustomerRequestDTO> customers);
+        public void Update(int id, CustomerRequestDTO customer);
+        public void Remove(int id);
     }
 }
