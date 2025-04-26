0fdb0f6b7abcebc9c67d6831e6df787207348714
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-04-26 10:54:37 -0300

    test: Adjustment in the third and fourth test of the ROUTE /Customers, I added the Addresses

--- src/DemoBackShopCore.Test/IntegrationTest.cs
+++ src/DemoBackShopCore.Test/IntegrationTest.cs
@@ -122,13 +122,28 @@ public class IntegrationTest : IClassFixture<WebApplicationFactory<Program>>
     public async Task AddCustomer_ReturnBadRequestLastNameIcorrect()
     {
         //Arrange
+        AddressRequestDTO address = new AddressRequestDTO
+        {
+            ZipCode = "A",
+            Street = "A",
+            Number = 0,
+            Neighborhood = "A",
+            AddressComplement = "A",
+            City = "A",
+            Country = "A",
+            State = "A",
+        };
+
         CustomerRequestDTO request = new CustomerRequestDTO
         {
             FirstName = "Yuri",
             LastName = "YuriYuriYuriYuriYuriYuriYuriYuriYuriYuriYuri",
             EmailAddress = "yuriB@exemplo.com",
-            DateOfBirth = DateTime.UtcNow.AddDays(-20)
+            DateOfBirth = DateTime.UtcNow.AddDays(-20),
+            Addresses = new List<AddressRequestDTO>()
         };
+
+        request.Addresses.Add(item: address);
             
         string json = JsonConvert.SerializeObject(request);
         StringContent content = new StringContent
@@ -154,13 +169,28 @@ public class IntegrationTest : IClassFixture<WebApplicationFactory<Program>>
     public async Task AddCustomer_ReturnConflict()
     {
         //Arrange
+        AddressRequestDTO address = new AddressRequestDTO
+        {
+            ZipCode = "A",
+            Street = "A",
+            Number = 0,
+            Neighborhood = "A",
+            AddressComplement = "A",
+            City = "A",
+            Country = "A",
+            State = "A",
+        };
+
         CustomerRequestDTO request = new CustomerRequestDTO
         {
             FirstName = "Yuri",
             LastName = "Torres",
             EmailAddress = "yuriA@exemplo.com",
-            DateOfBirth = DateTime.UtcNow.AddDays(-20)
+            DateOfBirth = DateTime.UtcNow.AddDays(-20),
+            Addresses = new List<AddressRequestDTO>()
         };
+
+        request.Addresses.Add(item: address);
             
         string json = JsonConvert.SerializeObject(request);
         StringContent content = new StringContent
