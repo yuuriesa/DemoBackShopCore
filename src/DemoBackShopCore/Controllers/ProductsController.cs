ef3a9a3de12859aaf0c515ad0438d8f09b276799
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-05-29 11:50:12 -0300

    feat: GetALL method added in Product Controller

--- src/DemoBackShopCore/Controllers/ProductsController.cs
+++ src/DemoBackShopCore/Controllers/ProductsController.cs
@@ -1,3 +1,5 @@
+using DemoBackShopCore.Models;
+using DemoBackShopCore.Utils;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DemoBackShopCore.Controllers
@@ -6,6 +8,18 @@ namespace DemoBackShopCore.Controllers
     [Route("api/Product")]
     public class ProductsController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult GetAll(int pageNumber = 1, int pageSize = 10)
+        {
+            if (pageNumber < 0 || pageSize < 0) return BadRequest(DomainResponseMessages.CustomerPaginationError);
 
+            PaginationFilter paginationFilter = new PaginationFilter(pageNumber: pageNumber, pageSize: pageSize);
+
+            //IQueryable<Product> products = ;
+
+            //if (products.Count() == 0) return NoContent();
+
+            return Ok();
+        }
     }
 }
