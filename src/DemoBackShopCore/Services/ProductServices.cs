a6ffd1888f917da2a3b571f6a065532309f7d820
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-05-29 12:21:44 -0300

    feat: GetAll method Added in ProdcutServices

--- src/DemoBackShopCore/Services/ProductServices.cs
+++ src/DemoBackShopCore/Services/ProductServices.cs
@@ -1,7 +1,13 @@
+using DemoBackShopCore.Models;
+using DemoBackShopCore.Utils;
+
 namespace DemoBackShopCore.Services
 {
     public class ProductServices : IProductServices
     {
-
+        public IQueryable<Product> GetAll(PaginationFilter paginationFilter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
