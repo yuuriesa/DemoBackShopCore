d7c19c6380715658beac7a858bf9e577d6df2ed6
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-06-03 13:42:20 -0300

    feat: added error message "ProductNotFound" to DomainsResponseMessages

--- src/DemoBackShopCore/utils/DomainResponseMessages.cs
+++ src/DemoBackShopCore/utils/DomainResponseMessages.cs
@@ -26,6 +26,7 @@ namespace DemoBackShopCore.Utils
 
         //Product
         public const string ProductIdMustBeGreaterThanZeroError = "ProductId must be greater than zero";
+        public const string ProductNotFoundMessageError = "Product not found";
         public const string ProductFieldsAreInvalidError = "Code or Name fields are invalid, check the values available.";
     }
 }
