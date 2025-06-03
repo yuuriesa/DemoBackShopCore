77c763188f7451c75162dd75eec7c3fbae08ded2
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-06-03 15:17:26 -0300

    feat: Added GetByCode to ProductRepository

--- src/DemoBackShopCore/Repository/ProductRepository.cs
+++ src/DemoBackShopCore/Repository/ProductRepository.cs
@@ -5,8 +5,22 @@ namespace DemoBackShopCore.Repository
 {
     public class ProductRepository : RepositoryBase<Product>, IProductRepository
     {
+        private readonly ApplicationDbContext _dbContext;
         public ProductRepository(ApplicationDbContext dbContext) : base(dbContext)
         {
+            _dbContext = dbContext;
+        }
+
+        public Product GetByCode(string code)
+        {
+            Product? product = _dbContext.Products.FirstOrDefault(p => p.Code == code);
+
+            if (product == null)
+            {
+                return null;
+            }
+
+            return product;
         }
     }
 }
