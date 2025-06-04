3bba09bcba8182b9840bd59b9640a79537234e3e
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-06-04 12:22:47 -0300

    feat: Add ProductResponseDTO

---
+++ src/DemoBackShopCore/DTOs/ProductResponseDTO.cs
@@ -0,0 +1,9 @@
+namespace DemoBackShopCore.DTOs
+{
+    public class ProductResponseDTO
+    {
+        public int ProductId { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+}
