d4914874ae775d2fdc2dbae6f567ab6093754f1e
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-06-11 12:59:56 -0300

    feat: Added method Delete for IProductServices

--- src/DemoBackShopCore/Services/IProductServices.cs
+++ src/DemoBackShopCore/Services/IProductServices.cs
@@ -13,6 +13,7 @@ namespace DemoBackShopCore.Services
         public Task<ServiceResult<List<Product>>> AddBatch(IEnumerable<ProductRequestDTO> productsRequestsDTO);
         public Task<ServiceResult<Batch2ResponseResult>> AddBatch2(IEnumerable<ProductRequestDTO> productsRequestsDTO);
         public ServiceResult<Product> Update(int id, ProductRequestDTO productRequestDTOforUpdate);
+        public ServiceResult<Product> Delete(int id);
         public IEnumerable<string> VerifyIfDuplicateCodes(IEnumerable<ProductRequestDTO> productsRequestsDTO);
         public ProductResponseDTO GenerateProductResponseDTO(Product product);
         public List<ProductResponseDTO> GenerateListProductResponseDTO(IQueryable<Product> products);
