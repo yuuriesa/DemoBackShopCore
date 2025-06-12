f0cbbef20bbf0ae788242785f13f9562c17a94b5
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-06-12 13:23:47 -0300

    feat: Created properties for Order model

--- src/DemoBackShopCore/Models/Order.cs
+++ src/DemoBackShopCore/Models/Order.cs
@@ -1,7 +1,23 @@
+using System.Text.Json.Serialization;
+
 namespace DemoBackShopCore.Models
 {
     public class Order
     {
-        
+        //private properties
+        private string _orderNumber { get; set; }
+        private DateTime _orderDate { get; set; }
+        private decimal _totalOrderValue { get; set; }
+        private int _customerId { get; set; }
+
+        //public properties
+        public int OrderId { get; private set; }
+        public string OrderNumber => _orderNumber;
+        public DateTime OrderDate => _orderDate;
+        public decimal TotalOrderValue => _totalOrderValue;
+        public int CustomerId => _customerId;
+        [JsonIgnore]
+        public Customer Customer { get; set; }
+        public ICollection<Item> Items { get; private set; } = new List<Item>();
     }
 }
