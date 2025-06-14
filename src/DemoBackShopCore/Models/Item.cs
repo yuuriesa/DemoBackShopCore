127ecd145818af504a78f9bdb1b17acb64b923f1
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-06-14 14:52:01 -0300

    feat: added properties public and private for Item model

--- src/DemoBackShopCore/Models/Item.cs
+++ src/DemoBackShopCore/Models/Item.cs
@@ -1,7 +1,26 @@
+using System.Text.Json.Serialization;
+
 namespace DemoBackShopCore.Models
 {
     public class Item
     {
-        
+        //private properties
+        private int _quantityOfItems { get; set; }
+        private decimal _unitValue { get; set; }
+
+
+        //public properties
+        public int ItemId { get; private set; }
+        public int QuantityOfItems => _quantityOfItems;
+        public decimal UnitValue => _unitValue;
+        public decimal TotalValue { get; private set; }
+        public int ProductId { get; private set; }
+        public int OrderId { get; private set; }
+        [JsonIgnore]
+        public Order Order { get; set; }
+        public string Code { get; private set; }
+
+        //properties for validation
+        public bool IsValid { get; private set; }
     }
 }
