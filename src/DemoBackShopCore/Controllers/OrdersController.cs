091c565f17addcdcbb4f6fecd5dba682484cac28
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-06-17 10:56:43 -0300

    feat: add file OrdersController

---
+++ src/DemoBackShopCore/Controllers/OrdersController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoBackShopCore.Controllers
+{
+    [ApiController]
+    [Route("api/Orders")]
+    public class OrdersController : ControllerBase
+    {
+        
+    }
+}
