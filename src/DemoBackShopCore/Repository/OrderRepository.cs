0ff90824af5b4edf86f75e6eec27ac24d5fd89d7
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-06-18 10:34:28 -0300

    feat: add private property dbcontext for OrderRepository

--- src/DemoBackShopCore/Repository/OrderRepository.cs
+++ src/DemoBackShopCore/Repository/OrderRepository.cs
@@ -5,8 +5,11 @@ namespace DemoBackShopCore.Repository
 {
     public class OrderRepository : RepositoryBase<Order>, IOrderRepository
     {
+        private readonly ApplicationDbContext _dbContext;
+
         public OrderRepository(ApplicationDbContext dbContext) : base(dbContext)
         {
+            _dbContext = dbContext;
         }
     }
 }
