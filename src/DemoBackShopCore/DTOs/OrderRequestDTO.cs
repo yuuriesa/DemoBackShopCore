062da2c4636708461899d3ed18d093d13aa5b715
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-06-23 19:06:07 -0300

    feat: Added and implemented OrderRequestDTO

--- src/DemoBackShopCore/DTOs/OrderRequestDTO.cs
+++ src/DemoBackShopCore/DTOs/OrderRequestDTO.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using DemoBackShopCore.Models;
+
 namespace DemoBackShopCore.DTOs
 {
     public class OrderRequestDTO
     {
-        
+        public string OrderNumber { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime OrderDate { get; set; }
+        public CustomerRequestDTO Customer { get; set; }
+        public List<ItemRequestDTO> Items { get; set; } = new List<ItemRequestDTO>();
     }
 }
