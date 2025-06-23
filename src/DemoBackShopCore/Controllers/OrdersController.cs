bdc234d63127ea91b0332dcf2d78b1fadcff7e41
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-06-23 18:31:48 -0300

    feat: add and implemented GetAll method for OrderController

--- src/DemoBackShopCore/Controllers/OrdersController.cs
+++ src/DemoBackShopCore/Controllers/OrdersController.cs
@@ -1,4 +1,7 @@
 using DemoBackShopCore.Data;
+using DemoBackShopCore.Models;
+using DemoBackShopCore.Services;
+using DemoBackShopCore.Utils;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DemoBackShopCore.Controllers
@@ -8,13 +11,30 @@ namespace DemoBackShopCore.Controllers
     public class OrdersController : ControllerBase
     {
         private readonly ApplicationDbContext _dbContext;
+        private readonly IOrderServices _services;
 
         public OrdersController
         (
-            ApplicationDbContext dbContext
+            ApplicationDbContext dbContext,
+            IOrderServices services
         )
         {
             _dbContext = dbContext;
+            _services = services;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll(int pageNumber = 1, int pageSize = 10)
+        {
+            if (pageNumber < 0 || pageSize < 0) return BadRequest(DomainResponseMessages.CustomerPaginationError);
+
+            PaginationFilter paginationFilter = new PaginationFilter(pageNumber: pageNumber, pageSize: pageSize);
+
+            IQueryable<Order> orders = _services.GetAll(paginationFilter: paginationFilter);
+
+            if (orders.Count() == 0) return NoContent();
+
+            return Ok(orders);
         }
     }
 }
