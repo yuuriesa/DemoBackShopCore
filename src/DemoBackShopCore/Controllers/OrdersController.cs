2552786d774538a6f2d059c9be1c1ea5288d4be5
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-06-23 18:44:14 -0300

    feat: added Add method for OrderController

--- src/DemoBackShopCore/Controllers/OrdersController.cs
+++ src/DemoBackShopCore/Controllers/OrdersController.cs
@@ -36,5 +36,11 @@ namespace DemoBackShopCore.Controllers
 
             return Ok(orders);
         }
+
+        [HttpPost]
+        public IActionResult Add()
+        {
+            return Ok();
+        }
     }
 }
