70e2f78a1e4f96c664ee51eba313d3ba07443040
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-06-29 09:39:18 -0300

    feat: add [Required] in properties(OrderRequestDTO)

--- src/DemoBackShopCore/DTOs/OrderRequestDTO.cs
+++ src/DemoBackShopCore/DTOs/OrderRequestDTO.cs
@@ -5,9 +5,12 @@ namespace DemoBackShopCore.DTOs
 {
     public class OrderRequestDTO
     {
+        [Required]
         public string OrderNumber { get; set; }
+        [Required]
         [DataType(DataType.Date)]
         public DateTime OrderDate { get; set; }
+        [Required]
         public CustomerRequestDTOForOrder Customer { get; set; }
         public List<ItemRequestDTO> Items { get; set; } = new List<ItemRequestDTO>();
     }
